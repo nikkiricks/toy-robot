837fecc49669d19436fbd6a996f5c7e0922d1986
nikkiricks <nikki.ricks@gmail.com>
2020-01-12 16:48:04 +1100

    adding rspec files for testing

---
+++ spec/toyrobot_spec.rb
@@ -0,0 +1,16 @@
+require './spec_helper'
+
+# require "toyrobot"
+
+describe "Toyrobot" do
+  
+  before :each do 
+    @new_game = Toyrobot.new "placement"
+  end
+  describe "#new" do
+    it "returns a new game object" do 
+      
+    end
+  end
+end
+
